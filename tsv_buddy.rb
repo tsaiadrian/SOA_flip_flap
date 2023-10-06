f17faaec5e18963df360e4dc774efa011dedddfd
107405026_蔡立恩 <107405026@nccu.edu.tw>
2023-10-06 14:56:24 +0800

    All tests pass; rubocop/flog resolved

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,18 +1,35 @@
 # frozen_string_literal: true
 
+TAB = "\t"
+NEWLINE = "\n"
+
 # Module that can be included (mixin) to take and output TSV data
 module TsvBuddy
+  attr_accessor :data
+
+  def initialize
+    @data
+  end
+
   # Converts a String with TSV data into internal data structure @data
   # arguments: tsv - a String in TSV format
   # returns: nothing
   def take_tsv(tsv)
+    rows = tsv.split(NEWLINE).map { |line| line.split(TAB) }
+    headers = rows.first
+    data_rows = rows[1..]
 
+    @data = data_rows.map do |row|
+      row.map.with_index { |cell, i| [headers[i], cell] }.to_h
+    end
   end
 
   # Converts @data into tsv string
   # arguments: none
   # returns: String in TSV format
   def to_tsv
-
+    headers = @data[0].keys.join(TAB)
+    rows = @data.map { |hash| hash.values.join(TAB) }.join(NEWLINE)
+    (headers + NEWLINE + rows + NEWLINE).to_s
   end
 end
--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,1 +1,20 @@
+# frozen_string_literal: true
+
+require 'yaml'
+
 # Module that can be included (mixin) to take and output Yaml data
+module YamlBuddy
+  attr_accessor :data
+
+  def initialize
+    @data
+  end
+
+  def take_yaml(yaml)
+    @data = YAML.safe_load(yaml)
+  end
+
+  def to_yaml
+    @data.to_yaml
+  end
+end
